797b9392764781608cfc19747fa509f2dbffe38a
Михаил Белов <mihailbelov@Mihails-MacBook-Pro.local>
2024-12-31 19:14:37 +0100

    html + styling

--- src/main.js
+++ src/main.js
@@ -15,30 +15,29 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(50);
+camera.position.setZ(42);
 
 // Creating objects
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
+torus.position.set(55, -15, -20);
 scene.add(torus);
 
 // PointList(light bolb)
 const pointLight = new THREE.PointLight(0xffffff, 1000);
 pointLight.position.set(20, 20, 10);
-
 // AmbientLight(lights everything equaly)
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(pointLight, ambientLight);
 
 // PointLightHelper(draws figure where pointLight is located)
-const pointLightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(pointLightHelper, gridHelper);
-
+// const pointLightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(pointLightHelper, gridHelper);
 // You can move view with your mouse, NOTE: requires updating in animate()
 // Requires renderer.domElement for listening dom events
-const orbitControls = new OrbitControls(camera, renderer.domElement);
+// const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -47,7 +46,6 @@ function addStar() {
   const [x, y, z] = Array(3)
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
-
   star.position.set(x, y, z);
   scene.add(star);
 }
@@ -57,17 +55,16 @@ for (let i = 0; i < 200; i++) {
 }
 
 // Loading image(texture)
-const spaceTexture = new THREE.TextureLoader().load("/space.jpg");
+const spaceTexture = new THREE.TextureLoader().load("../public/space.jpg");
 scene.background = spaceTexture;
-
 // Map for texture
 const reactTexture = new THREE.TextureLoader().load("/react.png");
 const react = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshStandardMaterial({ map: reactTexture })
 );
+react.position.set(55, -15, -20);
 scene.add(react);
-
 // Normal map for texture, NOTE: vectors bounce depending on rgb
 const moonTexture = new THREE.TextureLoader().load("/moon.jpg");
 const normalMap = new THREE.TextureLoader().load("/normal.jpg");
@@ -75,9 +72,23 @@ const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalMap })
 );
-moon.position.x = -10;
-moon.position.z = 30;
+moon.position.set(-17, 5, 25);
 scene.add(moon);
+
+// Create custom functionality on scrolling
+function moveCamera() {
+  // returns el.top - viewport.top
+  const rect = document.body.getBoundingClientRect();
+  const top = rect.top;
+  moon.rotation.x += 0.02;
+  moon.rotation.y += 0.035;
+  moon.rotation.z += 0.02;
+
+  react.rotation.x += top * -0.00006;
+  react.rotation.y += top * -0.00008;
+  react.rotation.z += top * -0.00008;
+}
+document.body.onscroll = moveCamera;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -86,7 +97,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  orbitControls.update();
+  // orbitControls.update();
 
   renderer.render(scene, camera);
 }
---
+++ vite.config.js
@@ -0,0 +1,4 @@
+// vite.config.js
+export default {
+  base: "/3d-portfolio-website/", // Change this to your repository name
+};
